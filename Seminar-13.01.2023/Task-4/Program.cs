64673e61f376bb4f97259acc2289505b58375284
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-14 19:32:36 +0300

    add Task-4

---
+++ Seminar-13.01.2023/Task-4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Эта программа принимает на вход число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+Console.Write("Введите, пожалуйста, N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(N + " -> ");
+
+if (N != 1)
+{
+    int count = 3;
+    Console.Write(" 2");
+    while (count <= N)
+    {    
+        if (count % 2 == 0) 
+        {
+            Console.Write(", ");
+            Console.Write(count);
+        }
+        count++;
+    }
+}    
