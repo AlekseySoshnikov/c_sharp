07069983a73a0815293406cd73ce9b68266e4ac1
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-14 13:45:53 +0300

    add Task-1

---
+++ Seminar-13.01.2023/Task-1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Эта программа принимает на вход два числа и выдаёт, какое число большее (max), а какое меньшее (min).");
+
+Console.Write("Введите, пожалуйста, первое число: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите, пожалуйста, второе число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 > number2) Console.WriteLine("Первое число больше второго числа: max=" + number1 + ", min =" + number2 + ".");
