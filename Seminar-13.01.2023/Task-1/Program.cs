ab45fb4fc8cc4cf74ba128ab767c840b855dd5ca
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-17 21:53:40 +0300

    modified Seminar-13.01.2023/Task-1/Program.cs

--- Seminar-13.01.2023/Task-1/Program.cs
+++ Seminar-13.01.2023/Task-1/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine("Эта программа принимает на вход два числа и выдаёт, какое число большее (max), а какое меньшее (min).");
+﻿Console.WriteLine("Эта программа принимает на вход два числа (a и b) и выдаёт, какое число большее (max), а какое меньшее (min).");
 
-Console.Write("Введите, пожалуйста, первое число: ");
-double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите, пожалуйста, числа, используя разделитель ',' для дробных:");
+Console.Write("a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Введите, пожалуйста, второе число: ");
-double number2 = Convert.ToDouble(Console.ReadLine());
-
-if (number1 > number2) Console.WriteLine("Первое число больше второго числа: max=" + number1 + ", min =" + number2 + ".");
+if (a > b) Console.WriteLine("Первое число больше второго числа: max = " + a + "; min = " + b + ".");
+if (b > a) Console.WriteLine("Второе число больше первого числа: max = " + b + "; min = " + a + ".");
+if (a == b) Console.WriteLine("Введённые числа равны, определить какое из них больше (max), а какое меньше (min) не представляется возможным.");
