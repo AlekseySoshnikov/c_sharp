48d01fac866b242435d2dec31870edda7fa779e4
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-18 18:22:57 +0300

    add Seminar-17.01.2023/Task-2v1

---
+++ Seminar-17.01.2023/Task-2v1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите, пожалуйста, число: ");
+string number = Console.ReadLine();
+
+Console.Write($"{number} -> ");
+
+if (number.Length < 3)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(number[2]);
+}
