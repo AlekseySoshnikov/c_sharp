a2a272875045711484149496e35a6de97c2a27f9
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-21 19:52:02 +0300

    add Seminar-20.01.2023/Task-1String

---
+++ Seminar-20.01.2023/Task-1String/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите, пожалуйста, число: ");
+string number = Console.ReadLine();
+
+Console.Write($"{number} -> ");
+
+for (int i = 0; i < number.Length / 2; i++)
+{
+    if (number[i] != number[number.Length - 1 + i])
+    {
+        System.Console.WriteLine("нет");
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine("да");
+    }
+}
