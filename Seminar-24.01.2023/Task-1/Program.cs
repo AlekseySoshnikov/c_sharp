a70b05dab5cee212166cc79a684636756f48c41c
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-24 21:44:32 +0300

    add Seminar-24.01.2023/Task-1

---
+++ Seminar-24.01.2023/Task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int A = GetRandomNumber();
+int B = GetRandomNumber();
+
+PrintResult(A,B,GetPow(A,B));
+
+int GetRandomNumber ()
+{
+    int number = new Random().Next(1,6);
+    return number;
+}
+
+int GetPow (int A, int B)
+{
+    int result = A;
+    for (int i = 2; i <= B; i++)
+    {
+        result *= A;
+    }
+
+    return result;
+}
+
+void PrintResult (int A, int B, int PowResult)
+{
+    System.Console.WriteLine($"{A}, {B} -> {PowResult}");
+}
