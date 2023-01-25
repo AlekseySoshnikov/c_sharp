66108506fc4ec0b0c85ea6880c552b4d1fa3e1a1
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-25 11:11:18 +0300

    add Seminar-24.01.2023/Task-3

---
+++ Seminar-24.01.2023/Task-3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int len = new Random().Next(0, 11);
+
+PrintResult(GetRandomArray(len));
+
+int[] GetRandomArray (int len)
+{
+    int[] res = new int[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        res[i] = new Random().Next(0, 100);
+    }
+
+    return res;
+}
+
+void PrintResult (int[] arr)
+{
+    for (int i = 0; i < arr.Length - 2; i++)
+    {
+        System.Console.Write(arr[i] + ", ");
+    }
+    System.Console.Write(arr[arr.Length - 1] + " -> [");
+
+    for (int i = 0; i < arr.Length - 2; i++)
+    {
+        System.Console.Write(arr[i] + ", ");
+    }
+    System.Console.Write(arr[arr.Length - 1] + "]");
+
+}
