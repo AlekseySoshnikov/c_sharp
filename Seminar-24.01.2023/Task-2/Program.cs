c01433ed5ac1d2db72bebab9e5686a33698a2c3c
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-25 10:38:16 +0300

    add Seminar-24.01.2023/Task-2

---
+++ Seminar-24.01.2023/Task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number = GetRandomNumber();
+
+PrintResult(number, SummingNumber(number));
+
+int SummingNumber (int number)
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number /= 10;
+    }
+
+    return result;
+}
+
+int GetRandomNumber ()
+{
+    int number = new Random().Next(1, 10000);
+
+    return number;
+}
+
+void PrintResult (int number, int result)
+{
+    System.Console.Write($"{number} -> {result}");
+}
