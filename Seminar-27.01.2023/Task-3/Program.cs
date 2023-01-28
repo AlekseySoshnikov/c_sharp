695133682ef2c921cd39fa2ea00aabec163c1a37
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-28 12:20:11 +0300

    Seminar-27.01.2023/Task-3

---
+++ Seminar-27.01.2023/Task-3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine($" -> {FindingDifference(arr, MaxIndexNumber(arr), MinIndexNumber(arr))}");
+
+int FindingDifference(int[] arr, int maxIndex, int minIndex)
+{
+    int dif = arr[maxIndex] - arr[minIndex];
+    return dif;
+}
+
+int MaxIndexNumber(int[] arr)
+{
+    int maxIndex = 0;
+    for(int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] > arr[maxIndex])
+        maxIndex = i;
+    }
+    return maxIndex;
+}
+
+int MinIndexNumber(int[] arr)
+{
+    int minIndex = 0;
+    for(int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] < arr[minIndex])
+        minIndex = i;
+    }
+    return minIndex;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
