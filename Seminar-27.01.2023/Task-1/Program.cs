03e6756da7a3387a5ebfb7fc68a98d1733c13734
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-01-28 11:39:16 +0300

    Seminar-27.01.2023/Task-1

---
+++ Seminar-27.01.2023/Task-1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine($" -> {CountingEvenNumbers(arr)}");
+
+int CountingEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
