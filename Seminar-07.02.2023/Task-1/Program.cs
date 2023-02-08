c91e0b779ff15d0488fe7a61e337d9022afa43b6
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-02-08 18:18:45 +0300

    add Seminar-07.02.2023/Task-1

---
+++ Seminar-07.02.2023/Task-1/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] arr = Generate2DArray(GetCountRow(), GetCountColumns());
+
+Print2DArray(arr);
+System.Console.WriteLine();
+Print2DArray(SortArray(arr));
+
+int[,] SortArray(int[,] arr)
+{
+    int[] temp = new int[arr.GetLength(1)];
+
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {        
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            temp[j] = arr[i, j];            
+        }
+
+        Array.Sort(temp);
+        Array.Reverse(temp);
+
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = temp[j];            
+        }
+    }
+    return arr;
+}
+
+int GetCountRow()
+{
+    System.Console.Write("Введите, пожалуйста, количество строк: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    return m;
+}
+
+int GetCountColumns()
+{
+    System.Console.Write("Введите, пожалуйста, количество столбцов: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    return n;
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return arr;
+}
+
+void Print2DArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
